4649e1584cf3b7e701c9d40b7f46d81829d2c3fe
Urvil Patel <urvilpatel@Urvils-MacBook-Pro.local>
2026-07-18 22:00:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
+import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
+import Final from './sections/Final';
+import Outro from './sections/Outro';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+
+      <FirstVideo />
+      <Jason />
+
+      <SecondVideo />
+      <Lucia />
+
+      <PostCard />
+      <Final />
+      <Outro />
+    </main>
+  )
+}
+
+export default App
